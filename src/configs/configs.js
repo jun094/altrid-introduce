f749aca94aeeced12d215c6170047d34d059a244
jun-young <jun094@naver.com>
2021-02-15 16:47:22 +0900

    config 제거

--- src/configs/configs.js
+++
@@ -1,6 +0,0 @@
-/** DEV or Prod 모드 설정 */
-const buildMode = 'prod';
-/** API 주소 */
-const apiUrl = `${buildMode === 'dev' ? 'http://localhost:3000' : 'https://lms.altridedge.com'}`;
-
-export { buildMode, apiUrl };
